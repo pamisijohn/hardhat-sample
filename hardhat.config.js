544f565ac591e0c9ced70aa3cd3a5b74d01ebba5
kayceekellz <kayceekellz@gmail.com>
2022-09-07 07:27:41 +0100

    File arranged

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+};
